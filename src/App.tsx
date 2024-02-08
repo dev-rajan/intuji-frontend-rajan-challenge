d5f59b2a3e645934e8699acdd20e407873273a19
Rajan Shrestha <rajansth77@gmail.com>
2024-02-08 22:07:19 +0545

    [chore] initial project setup, add ant design library

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "antd";
+import React from "react";
+
+const { Title } = Typography;
+
+const App: React.FC = () => (
+  <>
+    <Title>User Management</Title>
+  </>
+);
+
+export default App;
