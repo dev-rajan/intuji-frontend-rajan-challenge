98e66eea0ac0432d091e8299030110f3132da66f
Rajan Shrestha <rajansth77@gmail.com>
2024-02-08 22:47:48 +0545

    [chore]setup and connect firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Typography } from "antd";
+import { Button, Typography } from "antd";
+import { getDatabase, ref, set } from "firebase/database";
 import React from "react";
+
+import { app } from "./firebase";
 
 const { Title } = Typography;
 
-const App: React.FC = () => (
-  <>
-    <Title>User Management</Title>
-  </>
-);
+const db = getDatabase(app);
+
+const App: React.FC = () => {
+  const putData = () => {
+    set(ref(db, "users/"), {
+      id: 1,
+      name: "Rajan",
+      age: 30,
+    });
+  };
+  return (
+    <>
+      <Title>User Management</Title>
+      <Button onClick={putData} type="primary">
+        Test Firebase Connect
+      </Button>
+    </>
+  );
+};
 
 export default App;
